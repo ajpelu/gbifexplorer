fb08d90f19d4bd7b6ee26ea7f47fdf1768d953be
ajpelu <ajpelu@gmail.com>
2023-05-16 16:46:16 +0200

    update cov_taxo

--- R/cov_taxo.R
+++ R/cov_taxo.R
@@ -1,12 +1,25 @@
 #' taxonomic_cov
 #'
+#' It generates the taxonomic coverage of the dataset
 #' @param x A dataframe
-#' @param category taxonomic ranks defined by DarwinCore standard
+#' @param category a character vector with taxonomic ranks defined by
+#' [DarwinCore](https://dwc.tdwg.org/terms/) standard.
 #'
-#' @return
+#' @details It generates the taxonomic coverage of a occurrence dataset. For each
+#' category, the record numbers and relative frequencies of each taxa are computed. The
+#' category argument could be one of: `scientificName`, `kingdom`, `phylum`, `class`,
+#' `order`, `family`, `subfamily` and `genus`. For get coverage in all categories
+#' type `all`.
+#'
+#' @return A named list of tibbles summarizing the taxonomic coverage for each category.
+#' @examples
+#' # Example usage
+#' data(borreguiles)
+#' # Calculate taxonomic coverage for scientificName and genus
+#' taxonomic_cov(borreguiles, category = c("scientificName", "genus"))
+#'
 #' @export
 #'
-#' @examples
 taxonomic_cov <- function(x,
                           category = c(
                             "scientificName", "kingdom", "phylum", "class",
@@ -17,16 +30,25 @@ taxonomic_cov <- function(x,
     "order", "family", "subfamily", "genus", "all"
   )
 
-  if (category == "all") {
-    select_categories <- setdiff(all_categories, category)
-    select_categories <- select_categories[select_categories %in% names(occ)]
+  if (!inherits(x, "data.frame") && !inherits(x, "tbl_df")) {
+    stop("Input 'x' must be a data frame or tibble.")
+  }
+
+  if ("all" %in% category && length(category) > 1) {
+    stop("Cannot combine 'all' with other categories.\n Please select 'all' categories or a combination of several categories")
+  }
+
+  if ("all" %in% category) {
+    select_categories <- setdiff(all_categories, "all")
+    select_categories <- select_categories[select_categories %in% names(x)]
   } else {
     select_categories <- category
   }
 
   aux_summary <- function(y) {
     x |>
-      group_by(!!!syms(y)) |>
+      # group_by(!!!syms(y)) |>
+      group_by(across(all_of(y))) |>
       tally() |>
       mutate(freq = prop.table(n) * 100) |>
       arrange(desc(freq))
@@ -34,6 +56,6 @@ taxonomic_cov <- function(x,
 
   out <- select_categories |>
     purrr::map(aux_summary) |>
-    set_names(select_categories)
+    purrr::set_names(select_categories)
   return(out)
 }
